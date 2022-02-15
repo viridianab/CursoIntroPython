086137256a793ebb1042dd11800eda07a7045c17
viridianab <viridnb@gmail.com>
2022-02-15 01:02:59 -0800

    Katas resueltos

---
+++ Módulo 10/config.py
@@ -0,0 +1,15 @@
+
+# try:
+#     open("mars.jpg")
+# except FileNotFoundError as err:
+#     print("got a problem trying to read the file:", err)
+
+
+#try:
+#    open("config.txt")
+#except OSError as err:
+#    if err.errno == 2:
+#        print("Couldn't find the config.txt file!")
+#    elif err.errno == 13:
+#        print("Found config.txt but couldn't read it")
+
